12d386fd1369cbb157395e8b0c43873a34ca1a7c
seregatheone <sergeydemidovichwork@gmail.com>
2023-02-20 18:20:16 +0300

    CW-4: created splashscreen for different api versions

--- app/src/main/java/pat/project/coursework/MainActivity.kt
+++ app/src/main/java/pat/project/coursework/MainActivity.kt
@@ -3,12 +3,15 @@ package pat.project.coursework
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import pat.project.coursework.ui.theme.CourseWorkTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
+
             CourseWorkTheme {
                 // A surface container using the 'background' color from the theme
             }
