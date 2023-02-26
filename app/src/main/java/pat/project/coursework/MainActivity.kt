87b4177ef6da13af745f9229fa255de22ceb2c35
seregatheone <sergeydemidovichwork@gmail.com>
2023-02-26 18:45:42 +0300

    CW-3: created navigation

--- app/src/main/java/pat/project/coursework/MainActivity.kt
+++ app/src/main/java/pat/project/coursework/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CourseWorkTheme {
-                // A surface container using the 'background' color from the theme
+                Navigation()
             }
         }
     }
